e46bca36e870bbad5e505c2ffc4c700d96bcb55f
sdtcsyl <sdtcsyl@163.com>
2015-04-19 13:18:33 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,15 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## below are two functions that are used to create a special object that stores a matrix and caches its inversion.
 
 ## Write a short comment describing this function
+
 ## The first function, makeCacheMatrix creates a special “matrix",which is really a list containing a function to 
 ## 1. set the value of the matrix
 ## 2. get the value of the matrix
-## 3. set the value of the matrix
-## 4. get the value of the matrix
+## 3. set the value of the matrix inversion
+## 4. get the value of the matrix inversion
 makeCacheMatrix <- function(x=matrix()) {
   inv<-NULL;
   set<-function(y){
@@ -35,6 +37,7 @@ cacheSolve <- function(x, ...) {
   }else{
     ma<-invdata[1:nrow(invdata),1:nrow(invdata)];
   }
+  ## verifying if the matrix is invertible
   inv <- solve(ma,...);
   x$setinverse(inv);
   inv
